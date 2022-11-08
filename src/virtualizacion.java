e21fdfdb6e47f5b8a3fcee6e4ebc8ea250ebae7e
player1F <116390139+player1F@users.noreply.github.com>
2022-11-08 17:23:47 -0500

    Add files via upload

---
+++ src/virtualizacion.java
@@ -0,0 +1,10 @@
+public class virtualizacion {
+
+    public static void main(String[] args) {
+        System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES\n");
+        System.out.println("Implementacion de laboratorio multiusuario para practicas en un set de TV\n");
+        System.out.println("Implementacion de un videojuego para juventud idente\n");
+        System.out.println("Desarrollo de videojuegos en el marco de la materia juegos de la carrera de computacion\n");
+        System.out.println("Filtros en realidad aumentada\n");
+    }
+}
